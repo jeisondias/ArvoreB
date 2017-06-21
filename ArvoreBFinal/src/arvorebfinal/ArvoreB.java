35b97940e6ef3f18178d527cde6ce38e32093043
Jeison Dias <jeisondias@users.noreply.github.com>
2017-06-21 15:20:21 -0300

    Adição do método InserirNoNaoCheio na classe Árvore B

--- ArvoreBFinal/src/arvorebfinal/ArvoreB.java
+++ ArvoreBFinal/src/arvorebfinal/ArvoreB.java
@@ -69,6 +69,83 @@ public class ArvoreB {
         }
     }
     
+    /*
+      * Método para inserir na árvore;
+      * Checa se o nó está vazio, se estiver vazio, adiciona o valor
+      * Se não estiver vazio, verifica como está a raiz , divide o nó e insere no nó não cheio
+      * Realiza a divisão dos nós e adiciona em um nó não cheio a partir da raiz;
+      * Caso contrário, adiciona a partir da raiz;
+      * incrementa o número de elementos da árvore;
+    */
+    
+    public void inserir(int chave){
+        if(BuscaChave(raiz, chave) == null){
+            if(raiz.getN() == 0){
+                raiz.getChave().set(0, chave);
+                raiz.setN(raiz.getN()+1);
+            } else {
+                
+                No r = raiz;
+                if (r.getN() == ordem -1){
+                    No s = new No(ordem);
+                    raiz = s;
+                    s.setFolha(false);
+                    s.setN(0);
+                    s.getFilho().set(0, r);
+                    divideNo(s, 0, r);
+                    inserirNoNaoCheio(s, chave);
+                } else{
+                    inserirNoNaoCheio(r, chave);
+                }
+            }
+            nElementos++;
+        }
+        
+    }
+    
+    
+    /* 
+        * Método para realizar a inserção em um nó não cheio;
+        * noI = nó que será inserido e chave = chave que será inserida       
+        * Método para realizar a inserção em um nó não cheio;
+        * Método para realizar a inserção em um nó não cheio;
+        * Método para realizar a inserção em um nó não cheio;
+    */
+    public void inserirNoNaoCheio(No noI, int chave){
+        int i = noI.getN() - 1;
+        // Checa se o nó é uma folha e se for, pega a posição correta para inserir a chave
+        if(noI.isFolha()){
+            while (i >= 1 && chave < noI.getChave().get(i)) {
+                noI.getChave().set(i + 1, noI.getChave().get(i));
+                i--;
+            }
+            //Realiza a inserção da chave na posição i
+            i++;
+            noI.getChave().set(i, chave);
+            noI.setN(noI.getN()+ 1);
+            
+            // Se o nó não for folha, ele pega a posição correta para poder inserir a chave
+        } else {
+            while ((i >= 0 && chave < noI.getChave().get(i))){
+               i--;
+            }
+            //Se o filho estiver cheio, ele realiza a divisão
+            i++;
+            if ((noI.getFilho().get(i).getN() == ordem -1)){
+                divideNo(noI, i, noI.getFilho().get(i));
+                if(chave > noI.getChave().get(i)){
+                   i++;
+                }
+                // Realiza a inserção da chave no filho i do noI
+                inserirNoNaoCheio(noI.getFilho().get(i), chave);
+            }
+        }
+    
+    }
+    
+    
+    //Math.Floor retorna o maior número int que é menor ou igual ao argumento
+    public void divideNo(No noP, int i, No noF){}
   
 
 
